01bf7c39c83b1bee3a8222bb07ca9e65847baa5b
Ivan Petrov <vanespetroff@gmail.com>
2020-06-28 19:17:02 +0300

    some refactoring

--- main.js
+++ main.js
@@ -4,28 +4,11 @@ function CalculateClock() {
     for (let i = 1; i < 5; i++) {
         document.querySelector(".year" + i).textContent = tmp.toString()[i - 1];
     }
-    tmp = (curDate.getMonth() + 1).toString();
-    document.querySelector(".month1").textContent = tmp.length > 1 ? tmp[0] : 0;
-    document.querySelector(".month2").textContent = tmp.length > 1 ? tmp[1] : tmp[0];
-
-    tmp = curDate.getDate().toString();
-    document.querySelector(".day1").textContent = tmp.length > 1 ? tmp[0] : 0;
-    document.querySelector(".day2").textContent = tmp.length > 1 ? tmp[1] : tmp[0];
-
-    tmp = curDate.getHours().toString();
-    document.querySelector(".hour1").textContent = tmp.length > 1 ? tmp[0] : 0;
-    document.querySelector(".hour2").textContent = tmp.length > 1 ? tmp[1] : tmp[0];
-
-    tmp = curDate.getMinutes().toString();
-    document.querySelector(".minute1").textContent = tmp.length > 1 ? tmp[0] : 0;
-    document.querySelector(".minute2").textContent = tmp.length > 1 ? tmp[1] : tmp[0];
-
-    tmp = curDate.getSeconds().toString();
-    document.querySelector(".second1").textContent = tmp.length > 1 ? tmp[0] : 0;
-    document.querySelector(".second2").textContent = tmp.length > 1 ? tmp[1] : tmp[0];
-
-    console.log(curDate.getFullYear() + ":" + (curDate.getMonth() + 1) + ":" + curDate.getDate()
-        + "\\" + curDate.getHours() + ":" + curDate.getMinutes() + ":" + curDate.getSeconds());
+    UpdateClock(".month", curDate.getMonth() + 1);
+    UpdateClock(".day", curDate.getDate());
+    UpdateClock(".hour", curDate.getHours());
+    UpdateClock(".minute", curDate.getMinutes());
+    UpdateClock(".second", curDate.getSeconds());
 }
 
 function StartClock() {
@@ -34,6 +17,28 @@ function StartClock() {
 }
 
 function SetTimer() {
+    let timerTime = GetDate();
+
+    let id = setInterval(() => {
+        timerTime.second--;
+        if (timerTime.second <= 0 && timerTime.minute != 0) {
+            timerTime.second = 59;
+            timerTime.minute--;
+        }
+        else if (timerTime.minute == 0 && timerTime.hour != 0) {
+            timerTime.minute = 59;
+            timerTime.second = 59;
+            timerTime.hour--;
+        }
+        else if (timerTime.hour == 0 && timerTime.minute == 0 && timerTime.second == 0)
+            clearInterval(id);
+        UpdateTimer(".inputHour", timerTime.hour);
+        UpdateTimer(".inputMinute", timerTime.minute);
+        UpdateTimer(".inputSecond", timerTime.second);
+    }, 1000);
+}
+
+function GetDate() {
     let hours = document.querySelector(".inputHour1").value;
     hours = hours * 10 + document.querySelector(".inputHour2").value;
     let minutes = document.querySelector(".inputMinute1").value;
@@ -43,27 +48,28 @@ function SetTimer() {
     hours = hours == "" ? 0 : hours;
     minutes = minutes == "" ? 0 : minutes;
     seconds = seconds == "" ? 0 : seconds;
-    let id = setInterval(() => {
-        seconds--;
-        if (seconds <= 0 && minutes != 0) {
-            seconds = 59;
-            minutes--;
-        }
-        else if (minutes == 0 && hours != 0) {
-            minutes = 59;
-            seconds = 59;
-            hours--;
-        }
-        else if (hours == 0 && minutes == 0 && seconds == 0)
-            clearInterval(id);
-        document.querySelector(".inputHour1").value = hours.toString().length > 1 ? hours.toString()[0] : 0;
-        document.querySelector(".inputHour2").value = hours.toString().length > 1 ? hours.toString()[1] : hours.toString()[0];
-        document.querySelector(".inputMinute1").value = minutes.toString().length > 1 ? minutes.toString()[0] : 0;
-        document.querySelector(".inputMinute2").value = minutes.toString().length > 1 ? minutes.toString()[1] : minutes.toString()[0];
-        document.querySelector(".inputSecond1").value = seconds.toString().length > 1 ? seconds.toString()[0] : 0;
-        document.querySelector(".inputSecond2").value = seconds.toString().length > 1 ? seconds.toString()[1] : seconds.toString()[0];
-    }, 1000);
+    return new DateTime(0, 0, 0, hours, minutes, seconds);
+}
 
+function UpdateTimer(target, data) {
+    document.querySelector(target + "1").value = data.toString().length > 1 ? data.toString()[0] : 0;
+    document.querySelector(target + "2").value = data.toString().length > 1 ? data.toString()[1] : data.toString()[0];
+}
+
+function UpdateClock(target, data) {
+    document.querySelector(target + "1").textContent = data.toString().length > 1 ? data.toString()[0] : 0;
+    document.querySelector(target + "2").textContent = data.toString().length > 1 ? data.toString()[1] : data.toString()[0];
+}
+
+class DateTime {
+    constructor(year, month, day, hour, minute, second) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
 }
 
 StartClock();
